ba8a2e481aec37a8545b618c7bee47b182043ecb
HSM <iobincn@gmail.com>
2020-04-26 21:12:21 +0800

    CRC爆破png图片高度和宽度

--- Misc/crc32_png.py
+++ Misc/crc32_png.py
@@ -1,11 +1,26 @@
 import os
 import binascii
 import struct
-fileBath = ""
-misc = open(fileBath,"rb").read()
-for i in range(1024):
-    data = misc[12:16] + struct.pack('>i',i)+ misc[20:29]
-    crc32 = binascii.crc32(data) & 0xffffffff
-    if crc32 == 0x932f8a6b:
-        print(i)
-        print("hex:"+hex(i))
+filename = "flag.png"
+crc = 0x00000000
+download = "/home/hsm/Downloads/"
+filename = download+filename
+
+def width(filename,crc):
+    file = open(filename, "rb").read()
+    for i in range(1024):
+        data = file[12:16] + struct.pack('>i', i) + file[20:29]
+        crc32 = binascii.crc32(data) & 0xffffffff
+        if crc32 == crc:
+
+            print("width:" + hex(i))
+def heigh(filename,crc):
+    file = open(filename, "rb").read()
+    for i in range(1024):
+        data = file[12:20] + struct.pack('>i', i) + file[24:29]
+        crc32 = binascii.crc32(data) & 0xffffffff
+        if crc32 == crc:
+            print(i)
+            print("heigh:" + hex(i))
+width(filename,crc)
+heigh(filename,crc)
