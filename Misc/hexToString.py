1c4179bcc8540549824e24e2aad5c89e6a0d4f98
HSM <iobincn@gmail.com>
2020-10-13 19:58:55 +0800

    update

--- Misc/hexToString.py
+++ Misc/hexToString.py
@@ -1,8 +1,8 @@
-
+#! /usr/bin/env python3
 
 import re
 str1 = "64010602010000000014"
-
+str1 = "79616E7A69205A4A517B78696C7A765F6971737375686F635F73757A6A677D20"
 
 def hexToString(string):
     arr = re.findall(r'.{2}',string)
